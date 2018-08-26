813a607708bfd3f746d874e8b1b6dda50f38ea9d
Christopher Armstrong <radix@twistedmatrix.com>
2018-08-26 10:19:56 -0500

    `let` works

--- src/lib.rs
+++ src/lib.rs
@@ -33,32 +33,33 @@ pub fn eval(form: &Expr, env: &mut Env) -> Result<Rc<SLVal>, String> {
             .get(s)
             .ok_or_else(|| "Variable not found".to_string())?
             .clone()),
-        // AValue::Cons(left, right) => match *left {
-        //     AValue::Symbol(s) => match s.as_ref() {
-        //         "let" => {
-        //             match *right {
-        //                 AValue::Cons(var, box_cons_expr) => match *var {
-        //                     AValue::Symbol(variable_name) => match *box_cons_expr {
-        //                         AValue::Cons(box_expr, box_hopeful_nil) => match *box_hopeful_nil {
-        //                             AValue::Nil => {
-        //                                 let result = eval(box_expr, env)?;
-        //                                 env.insert(variable_name.clone(), result);
-        //                                 Ok(Rc::new(SLVal::Void))
-        //                             },
-        //                             _ => Err("`let` must have two arguments.".to_string())
-        //                         }
-        //                         _ => Err("`let` must have two arguments.".to_string()),
-        //                     },
-        //                     _ => Err("first argument to `let` must be a symbol".to_string()),
-        //                 },
-        //                 _ => Err("`let` must have two arguments, a symbol and an expression."
-        //                     .to_string()),
-        //             }
-        //         }
-        //         _ => Err("Bad special form".to_string()),
-        //     },
-        //     _ => Err("callables must be symbols for now".to_string()),
-        // },
+        AValue::Cons(left, right) => match **left {
+            AValue::Symbol(ref s) => match s.as_ref() {
+                "let" => match **right {
+                    AValue::Cons(ref var, ref box_cons_expr) => match **var {
+                        AValue::Symbol(ref variable_name) => match **box_cons_expr {
+                            AValue::Cons(ref box_expr, ref box_hopeful_nil) => {
+                                match **box_hopeful_nil {
+                                    AValue::Nil => {
+                                        let result = eval(box_expr, env)?;
+                                        env.insert(variable_name.clone(), result);
+                                        Ok(Rc::new(SLVal::Void))
+                                    }
+                                    _ => Err("`let` must have two arguments.".to_string()),
+                                }
+                            }
+                            _ => Err("`let` must have two arguments.".to_string()),
+                        },
+                        _ => Err("first argument to `let` must be a symbol".to_string()),
+                    },
+                    _ => Err(
+                        "`let` must have two arguments, a symbol and an expression.".to_string()
+                    ),
+                },
+                _ => Err("Bad special form".to_string()),
+            },
+            _ => Err("callables must be symbols for now".to_string()),
+        },
         _ => Err("Sorry unimplement".to_string()),
     }
 }
@@ -85,7 +86,10 @@ mod test {
         let form = Parser::new(&"64").parse_basic().unwrap();
         assert_eq!(eval(&form, &mut empty).unwrap(), Rc::new(SLVal::Int(64)));
         let form = Parser::new(&"64.0").parse_basic().unwrap();
-        assert_eq!(eval(&form, &mut empty).unwrap(), Rc::new(SLVal::Float(64.0)));
+        assert_eq!(
+            eval(&form, &mut empty).unwrap(),
+            Rc::new(SLVal::Float(64.0))
+        );
     }
 
     #[test]
@@ -110,7 +114,7 @@ mod test {
             Rc::new(SLVal::String("my value".to_string())),
         );
         let form = form("(let x 5)");
-        eval(&form, &mut env);
+        eval(&form, &mut env).unwrap();
         assert_eq!(env.get("x").unwrap(), &Rc::new(SLVal::Int(5)));
     }
 }
