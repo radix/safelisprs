3ea3c6dc570dd00549d3d716c9d47d5aa93dc932
Christopher Armstrong <radix@twistedmatrix.com>
2018-08-26 09:30:07 -0500

    variables and reference counting

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 extern crate atoms;
 use atoms::Value as AValue;
 
+use std::collections::HashMap;
+use std::rc::Rc;
+
 type Expr = AValue<String>;
+type Env = HashMap<String, Rc<SLVal>>;
 
 #[derive(Debug, PartialEq)]
 pub enum SLVal {
@@ -12,24 +16,47 @@ pub enum SLVal {
     List(Vec<SLVal>),
 }
 
-pub fn eval(form: Expr) -> Result<SLVal, String> {
+pub fn eval(form: Expr, env: &mut Env) -> Result<Rc<SLVal>, String> {
     match form {
-        AValue::Str(s) => Ok(SLVal::String(s)),
-        // AValue::Symbol(s) => Ok(SLVal::Symbol(s)),
-        AValue::Int(i) => Ok(SLVal::Int(i)),
-        AValue::Float(f) => Ok(SLVal::Float(f)),
+        AValue::Str(s) => Ok(Rc::new(SLVal::String(s))),
+        AValue::Int(i) => Ok(Rc::new(SLVal::Int(i))),
+        AValue::Float(f) => Ok(Rc::new(SLVal::Float(f))),
+        AValue::Symbol(s) =>
+            Ok(env.get(&s).ok_or_else(|| "Variable not found".to_string())?.clone()),
         _ => Err("Sorry unimplement".to_string()),
     }
 }
 
+#[cfg(test)]
+mod test {
 
-#[test]
-fn atoms() {
     use atoms::Parser;
-    let form = Parser::new(&"\"foo\"").parse_basic().unwrap();
-    assert_eq!(eval(form).unwrap(), SLVal::String("foo".to_string()));
-    let form = Parser::new(&"64").parse_basic().unwrap();
-    assert_eq!(eval(form).unwrap(), SLVal::Int(64));
-    let form = Parser::new(&"64.0").parse_basic().unwrap();
-    assert_eq!(eval(form).unwrap(), SLVal::Float(64.0));
+    use super::*;
+
+    fn form(inp: &str) -> Expr {
+        Parser::new(&inp).parse_basic().unwrap()
+    }
+
+    #[test]
+    fn atoms() {
+        let mut empty = HashMap::new();
+
+        let form = form("\"foo\"");
+        assert_eq!(
+            eval(form, &mut empty).unwrap(),
+            Rc::new(SLVal::String("foo".to_string()))
+        );
+        let form = Parser::new(&"64").parse_basic().unwrap();
+        assert_eq!(eval(form, &mut empty).unwrap(), Rc::new(SLVal::Int(64)));
+        let form = Parser::new(&"64.0").parse_basic().unwrap();
+        assert_eq!(eval(form, &mut empty).unwrap(), Rc::new(SLVal::Float(64.0)));
+    }
+
+    #[test]
+    fn variable() {
+        let mut env = HashMap::new();
+        env.insert("myvar".to_string(), Rc::new(SLVal::String("my value".to_string())));
+        let form = form("myvar");
+        assert_eq!(eval(form, &mut env).unwrap(), Rc::new(SLVal::String("my value".to_string())));
+    }
 }
