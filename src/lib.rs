9f33b977f811fb8bda3bfe4c3656d02fcb2bd7f6
Christopher Armstrong <radix@twistedmatrix.com>
2018-08-26 10:30:59 -0500

    refactor special_let out of eval

--- src/lib.rs
+++ src/lib.rs
@@ -35,32 +35,33 @@ pub fn eval(form: &Expr, env: &mut Env) -> Result<Rc<SLVal>, String> {
             .clone()),
         AValue::Cons(left, right) => match **left {
             AValue::Symbol(ref s) => match s.as_ref() {
-                "let" => match **right {
-                    AValue::Cons(ref var, ref box_cons_expr) => match **var {
-                        AValue::Symbol(ref variable_name) => match **box_cons_expr {
-                            AValue::Cons(ref box_expr, ref box_hopeful_nil) => {
-                                match **box_hopeful_nil {
-                                    AValue::Nil => {
-                                        let result = eval(box_expr, env)?;
-                                        env.insert(variable_name.clone(), result);
-                                        Ok(Rc::new(SLVal::Void))
-                                    }
-                                    _ => Err("`let` must have two arguments.".to_string()),
-                                }
-                            }
-                            _ => Err("`let` must have two arguments. Don't use a dot.".to_string()),
-                        },
-                        _ => Err("first argument to `let` must be a symbol".to_string()),
-                    },
-                    _ => Err(
-                        "`let` must have two arguments, a symbol and an expression.".to_string()
-                    ),
-                },
+                "let" => special_let(right, env),
                 _ => Err("Unknown special form".to_string()),
             },
             _ => Err("callables must be symbols for now".to_string()),
         },
         _ => Err("Sorry unimplement".to_string()),
+    }
+}
+
+// why is this box a reference
+fn special_let(right: &Box<Expr>, env: &mut Env) -> Result<Rc<SLVal>, String> {
+    match **right {
+        AValue::Cons(ref var, ref box_cons_expr) => match **var {
+            AValue::Symbol(ref variable_name) => match **box_cons_expr {
+                AValue::Cons(ref box_expr, ref box_hopeful_nil) => match **box_hopeful_nil {
+                    AValue::Nil => {
+                        let result = eval(box_expr, env)?;
+                        env.insert(variable_name.clone(), result);
+                        Ok(Rc::new(SLVal::Void))
+                    }
+                    _ => Err("`let` must have two arguments.".to_string()),
+                },
+                _ => Err("`let` must have two arguments. Don't use a dot.".to_string()),
+            },
+            _ => Err("first argument to `let` must be a symbol".to_string()),
+        },
+        _ => Err("`let` must have two arguments, a symbol and an expression.".to_string()),
     }
 }
 
