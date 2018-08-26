6cbd981bc9e5e2fe663d00e75bb686bfe7ff299f
Christopher Armstrong <radix@twistedmatrix.com>
2018-08-26 10:13:48 -0500

    switch eval to accepting form as an expr.
    and add a bunch of commented-out code for supporting `let`.

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ extern crate atoms;
 use atoms::Value as AValue;
 
 use std::collections::HashMap;
-use std::rc::Rc;
+use std::rc::Rc; // TODO: use Manishearth/rust-gc
 
 type Expr = AValue<String>;
 type Env = HashMap<String, Rc<SLVal>>;
@@ -14,15 +14,51 @@ pub enum SLVal {
     String(String),
     Symbol(String),
     List(Vec<SLVal>),
+    Void,
 }
 
-pub fn eval(form: Expr, env: &mut Env) -> Result<Rc<SLVal>, String> {
+// pub enum AST {
+//     Let(Variable, AST),
+//     Call(Expr, Vec<Expr>),
+//     Variable(String),
+//     Int, Float, String, List,
+// }
+
+pub fn eval(form: &Expr, env: &mut Env) -> Result<Rc<SLVal>, String> {
     match form {
-        AValue::Str(s) => Ok(Rc::new(SLVal::String(s))),
-        AValue::Int(i) => Ok(Rc::new(SLVal::Int(i))),
-        AValue::Float(f) => Ok(Rc::new(SLVal::Float(f))),
-        AValue::Symbol(s) =>
-            Ok(env.get(&s).ok_or_else(|| "Variable not found".to_string())?.clone()),
+        AValue::Str(s) => Ok(Rc::new(SLVal::String(s.clone()))),
+        AValue::Int(i) => Ok(Rc::new(SLVal::Int(i.clone()))),
+        AValue::Float(f) => Ok(Rc::new(SLVal::Float(f.clone()))),
+        AValue::Symbol(s) => Ok(env
+            .get(s)
+            .ok_or_else(|| "Variable not found".to_string())?
+            .clone()),
+        // AValue::Cons(left, right) => match *left {
+        //     AValue::Symbol(s) => match s.as_ref() {
+        //         "let" => {
+        //             match *right {
+        //                 AValue::Cons(var, box_cons_expr) => match *var {
+        //                     AValue::Symbol(variable_name) => match *box_cons_expr {
+        //                         AValue::Cons(box_expr, box_hopeful_nil) => match *box_hopeful_nil {
+        //                             AValue::Nil => {
+        //                                 let result = eval(box_expr, env)?;
+        //                                 env.insert(variable_name.clone(), result);
+        //                                 Ok(Rc::new(SLVal::Void))
+        //                             },
+        //                             _ => Err("`let` must have two arguments.".to_string())
+        //                         }
+        //                         _ => Err("`let` must have two arguments.".to_string()),
+        //                     },
+        //                     _ => Err("first argument to `let` must be a symbol".to_string()),
+        //                 },
+        //                 _ => Err("`let` must have two arguments, a symbol and an expression."
+        //                     .to_string()),
+        //             }
+        //         }
+        //         _ => Err("Bad special form".to_string()),
+        //     },
+        //     _ => Err("callables must be symbols for now".to_string()),
+        // },
         _ => Err("Sorry unimplement".to_string()),
     }
 }
@@ -30,8 +66,8 @@ pub fn eval(form: Expr, env: &mut Env) -> Result<Rc<SLVal>, String> {
 #[cfg(test)]
 mod test {
 
-    use atoms::Parser;
     use super::*;
+    use atoms::Parser;
 
     fn form(inp: &str) -> Expr {
         Parser::new(&inp).parse_basic().unwrap()
@@ -43,20 +79,38 @@ mod test {
 
         let form = form("\"foo\"");
         assert_eq!(
-            eval(form, &mut empty).unwrap(),
+            eval(&form, &mut empty).unwrap(),
             Rc::new(SLVal::String("foo".to_string()))
         );
         let form = Parser::new(&"64").parse_basic().unwrap();
-        assert_eq!(eval(form, &mut empty).unwrap(), Rc::new(SLVal::Int(64)));
+        assert_eq!(eval(&form, &mut empty).unwrap(), Rc::new(SLVal::Int(64)));
         let form = Parser::new(&"64.0").parse_basic().unwrap();
-        assert_eq!(eval(form, &mut empty).unwrap(), Rc::new(SLVal::Float(64.0)));
+        assert_eq!(eval(&form, &mut empty).unwrap(), Rc::new(SLVal::Float(64.0)));
     }
 
     #[test]
     fn variable() {
         let mut env = HashMap::new();
-        env.insert("myvar".to_string(), Rc::new(SLVal::String("my value".to_string())));
+        env.insert(
+            "myvar".to_string(),
+            Rc::new(SLVal::String("my value".to_string())),
+        );
         let form = form("myvar");
-        assert_eq!(eval(form, &mut env).unwrap(), Rc::new(SLVal::String("my value".to_string())));
+        assert_eq!(
+            eval(&form, &mut env).unwrap(),
+            Rc::new(SLVal::String("my value".to_string()))
+        );
+    }
+
+    #[test]
+    fn let_var() {
+        let mut env = HashMap::new();
+        env.insert(
+            "myvar".to_string(),
+            Rc::new(SLVal::String("my value".to_string())),
+        );
+        let form = form("(let x 5)");
+        eval(&form, &mut env);
+        assert_eq!(env.get("x").unwrap(), &Rc::new(SLVal::Int(5)));
     }
 }
