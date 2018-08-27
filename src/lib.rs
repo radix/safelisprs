e8e1b3001b99dad48b101630d4ee1ede4a2b3104
Christopher Armstrong <radix@twistedmatrix.com>
2018-08-26 22:50:04 -0500

    wow, I implemented an AST! That was quick

--- src/lib.rs
+++ src/lib.rs
@@ -80,62 +80,139 @@ pub enum SLVal {
 pub struct Function {
     name: String,
     params: Vec<String>,
-    body: Expr,
+    body: AST,
 }
 
-// TODO: Using s-expressions for the interpreter sucks. We should parse the
-// s-expressions into a higher-level AST, like this:
-//
-//     pub enum AST {
-//         Let(Variable, AST),
-//         Call(Expr, Vec<Expr>),
-//         Variable(String),
-//         Int, Float, String, List,
-//     }
+#[derive(Debug, PartialEq, Clone)]
+pub enum AST {
+    Let(String, Box<AST>),
+    DefineFn(String, Vec<String>, Box<AST>),
+    Call(Box<AST>, Vec<AST>),
+    Variable(String),
+    Int(i64),
+    Float(f64),
+    String(String),
+}
 
-pub fn eval(form: &Expr, env: &mut Env) -> Result<Rc<SLVal>, String> {
+impl AST {
+    pub fn from_atoms(form: &Expr) -> Result<Self, String> {
+        match form {
+            AValue::Str(s) => Ok(AST::String(s.clone())),
+            AValue::Int(i) => Ok(AST::Int(i.clone())),
+            AValue::Float(f) => Ok(AST::Float(f.clone())),
+            AValue::Symbol(s) => Ok(AST::Variable(s.clone())),
+            AValue::Cons(left, right) => {
+                match **left {
+                    AValue::Symbol(ref s) => match s.as_ref() {
+                        "let" => return parse_let(right),
+                        "fn" => return parse_fn(right),
+                        _ => {}
+                    },
+                    _ => {}
+                }
+                let function_ast = AST::from_atoms(left)?;
+                let args: Result<Vec<AST>, String> = flatten_list(right)?
+                    .iter()
+                    .map(|form| AST::from_atoms(form))
+                    .collect();
+                let args = args?;
+                Ok(AST::Call(Box::new(function_ast), args))
+            }
+            _ => Err(format!("Sorry, didn't implement {:?} yet", form)),
+        }
+    }
+}
+
+fn parse_let(right: &Box<Expr>) -> Result<AST, String> {
+    match **right {
+        AValue::Cons(ref var, ref box_cons_expr) => match **var {
+            AValue::Symbol(ref variable_name) => match **box_cons_expr {
+                AValue::Cons(ref box_expr, ref box_hopeful_nil) => match **box_hopeful_nil {
+                    AValue::Nil => Ok(AST::Let(
+                        variable_name.clone(),
+                        Box::new(AST::from_atoms(box_expr)?),
+                    )),
+                    _ => Err("`let` must have two arguments.".to_string()),
+                },
+                _ => Err("`let` must have two arguments. Don't use a dot.".to_string()),
+            },
+            _ => Err("first argument to `let` must be a symbol".to_string()),
+        },
+        _ => Err("`let` must have two arguments, a symbol and an expression.".to_string()),
+    }
+}
+
+fn parse_fn(right: &Box<Expr>) -> Result<AST, String> {
+    match **right {
+        AValue::Cons(ref name, ref box_cons_params_and_body) => match **name {
+            AValue::Symbol(ref name) => match **box_cons_params_and_body {
+                AValue::Cons(ref params, ref box_cons_body_and_nil) => {
+                    match **box_cons_body_and_nil {
+                        AValue::Cons(ref body, _) => {
+                            let flattened_params = flatten_list(params)?;
+                            let mut params_vec = vec![];
+                            for param_expr in flattened_params {
+                                if let AValue::Symbol(p) = param_expr {
+                                    params_vec.push(p);
+                                } else {
+                                    return Err("Parameters must be symbols".to_string());
+                                }
+                            }
+                            Ok(AST::DefineFn(
+                                name.clone(),
+                                params_vec,
+                                Box::new(AST::from_atoms(body)?),
+                            ))
+                        }
+                        _ => Err("bad `fn`".to_string()),
+                    }
+                }
+                _ => Err("`fn` must take parameters after the name.".to_string()),
+            },
+            _ => Err("`fn` first argument must be a symbol.".to_string()),
+        },
+        _ => Err("`fn` must take parameters. And don't use a dot.".to_string()),
+    }
+}
+
+pub fn eval(form: &AST, env: &mut Env) -> Result<Rc<SLVal>, String> {
     match form {
-        AValue::Str(s) => Ok(Rc::new(SLVal::String(s.clone()))),
-        AValue::Int(i) => Ok(Rc::new(SLVal::Int(i.clone()))),
-        AValue::Float(f) => Ok(Rc::new(SLVal::Float(f.clone()))),
-        AValue::Symbol(s) => Ok(env
+        AST::String(s) => Ok(Rc::new(SLVal::String(s.clone()))),
+        AST::Int(i) => Ok(Rc::new(SLVal::Int(i.clone()))),
+        AST::Float(f) => Ok(Rc::new(SLVal::Float(f.clone()))),
+        AST::Variable(s) => Ok(env
             .get(s)
             .ok_or_else(|| "Variable not found".to_string())?
             .clone()),
-        AValue::Cons(left, right) => {
-            match **left {
-                AValue::Symbol(ref s) => match s.as_ref() {
-                    "let" => return special_let(right, env),
-                    "fn" => return special_fn(right, env),
-                    _ => {}
-                },
-                _ => {}
-            }
-            let func = eval(left, env)?;
-            if let SLVal::Function(ref func) = *func {
-                let args_unevaled = flatten_list(right)?;
-                call_fn(func, args_unevaled, env)
-            } else {
-                Err(format!("Tried to call a non-function: {:?}", func))
-            }
-        }
-        _ => Err("Sorry unimplement".to_string()),
+        AST::Let(name, expr) => special_let(name, expr, env),
+        AST::DefineFn(name, params, body) => special_fn(name, params, body, env),
+        AST::Call(func, args) => call_fn(func, args, env),
     }
 }
 
-fn call_fn(func: &Function, args: Vec<Expr>, env: &mut Env) -> Result<Rc<SLVal>, String> {
-    if args.len() != func.params.len() {
-        return Err(format!("{} requires {} arguments, found {}", func.name, func.params.len(), args.len()));
+fn call_fn(func: &AST, args: &Vec<AST>, env: &mut Env) -> Result<Rc<SLVal>, String> {
+    let func = eval(func, env)?;
+    if let SLVal::Function(ref func) = *func {
+        if args.len() != func.params.len() {
+            return Err(format!(
+                "{} requires {} arguments, found {}",
+                func.name,
+                func.params.len(),
+                args.len()
+            ));
+        }
+        let evaled: Vec<_> = args.iter().map(|a| eval(a, env)).collect();
+        // 3. bind arguments to parameters
+        env.push();
+        for (name, arg) in func.params.iter().zip(evaled) {
+            env.set(name.to_string(), arg?);
+        }
+        let result = eval(&func.body, env);
+        env.pop()?;
+        result
+    } else {
+        Err(format!("Tried to call a non-function: {:?}", func))
     }
-    let evaled: Vec<_> = args.iter().map(|a| eval(a, env)).collect();
-    // 3. bind arguments to parameters
-    env.push();
-    for (name, arg) in func.params.iter().zip(evaled) {
-        env.set(name.to_string(), arg?);
-    }
-    let result = eval(&func.body, env);
-    env.pop()?;
-    result
 }
 
 fn flatten_list(mut cons: &AValue<String>) -> Result<Vec<Expr>, String> {
@@ -152,62 +229,26 @@ fn flatten_list(mut cons: &AValue<String>) -> Result<Vec<Expr>, String> {
     }
 }
 
-fn special_fn(right: &Box<Expr>, env: &mut Env) -> Result<Rc<SLVal>, String> {
-    match **right {
-        AValue::Cons(ref name, ref box_cons_params_and_body) => match **name {
-            AValue::Symbol(ref name) => match **box_cons_params_and_body {
-                AValue::Cons(ref params, ref box_cons_body_and_nil) => {
-                    match **box_cons_body_and_nil {
-                        AValue::Cons(ref body, _) => {
-                            let flattened_params = flatten_list(params)?;
-                            let mut params_vec = vec![];
-                            for param_expr in flattened_params {
-                                if let AValue::Symbol(p) = param_expr {
-                                    params_vec.push(p);
-                                } else {
-                                    return Err("Parameters must be symbols".to_string());
-                                }
-                            }
-                            let func = SLVal::Function(Function {
-                                name: name.clone(),
-                                params: params_vec,
-                                body: *body.clone(),
-                            });
-                            env.set(name.clone(), Rc::new(func));
-                            return Ok(Rc::new(SLVal::Void));
-                        }
-                        _ => Err("bad `fn`".to_string()),
-                    }
-                }
-                _ => Err("`fn` must take parameters after the name.".to_string()),
-            },
-            _ => Err("`fn` first argument must be a symbol.".to_string()),
-        },
-        _ => Err("`fn` must take parameters. And don't use a dot.".to_string()),
-    }
+fn special_fn(
+    name: &str,
+    params: &Vec<String>,
+    body: &AST,
+    env: &mut Env,
+) -> Result<Rc<SLVal>, String> {
+    let func = SLVal::Function(Function {
+        name: name.to_string(),
+        params: params.clone(),
+        body: body.clone(),
+    });
+    env.set(name.to_string(), Rc::new(func));
+    return Ok(Rc::new(SLVal::Void));
 }
 
 // why is this box a reference
-fn special_let(right: &Box<Expr>, env: &mut Env) -> Result<Rc<SLVal>, String> {
-    // Would if_chain! help here? It would mean we would have less specific
-    // error messages. I probably need a custom pattern-matcher.
-    match **right {
-        AValue::Cons(ref var, ref box_cons_expr) => match **var {
-            AValue::Symbol(ref variable_name) => match **box_cons_expr {
-                AValue::Cons(ref box_expr, ref box_hopeful_nil) => match **box_hopeful_nil {
-                    AValue::Nil => {
-                        let result = eval(box_expr, env)?;
-                        env.set(variable_name.clone(), result);
-                        Ok(Rc::new(SLVal::Void))
-                    }
-                    _ => Err("`let` must have two arguments.".to_string()),
-                },
-                _ => Err("`let` must have two arguments. Don't use a dot.".to_string()),
-            },
-            _ => Err("first argument to `let` must be a symbol".to_string()),
-        },
-        _ => Err("`let` must have two arguments, a symbol and an expression.".to_string()),
-    }
+fn special_let(name: &str, expr: &AST, env: &mut Env) -> Result<Rc<SLVal>, String> {
+    let result = eval(expr, env)?;
+    env.set(name.to_string(), result);
+    Ok(Rc::new(SLVal::Void))
 }
 
 #[cfg(test)]
@@ -216,8 +257,8 @@ mod test {
     use super::*;
     use atoms::Parser;
 
-    fn form(inp: &str) -> Expr {
-        Parser::new(&inp).parse_basic().unwrap()
+    fn form(inp: &str) -> AST {
+        AST::from_atoms(&Parser::new(&inp).parse_basic().unwrap()).unwrap()
     }
 
     #[test]
