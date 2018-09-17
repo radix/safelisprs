2db1e7e287528821a1b31012b2faf456d6c68949
Christopher Armstrong <radix@twistedmatrix.com>
2018-09-16 19:08:07 -0500

    docstrings inside functions! woo!

--- src/transforms.rs
+++ src/transforms.rs
@@ -41,23 +41,24 @@ foo:
 
 use parser::{Function, AST};
 
-/// We transform any usage of closures (i.e., nested functions that make use of
-/// variables from their lexical scope) into mostly plain functions.
-///
-/// 1. nested functions (regardless of whether they use enclosing variables) are
-///    lifted (I'm not sure if this is actually an important thing to happen at
-///    the AST level, but it makes things easier).
-/// 2. variables used in closures have their values wrapped in Cell, and their
-///    usage wrapped in DerefCell.
-/// 3. Free variables in closure-functions get converted to parameters, which
-///    are expected to be passed as Cells.
-/// 4. We set a local variable with the result of a MakeClosure where the inner
-///    function was defined.
-///
-/// TODO: Disallow any invocation or returning of a closure before all of its
-/// cells have been initialized.
-/// TODO: "drop" the (let cname (make-closure ...)) to the lowest point possible
 pub fn closure_transform(items: &[AST]) -> Result<Vec<AST>, String> {
+  //! We transform any usage of closures (i.e., nested functions that make use of
+  //! variables from their lexical scope) into mostly plain functions.
+  //!
+  //! 1. nested functions (regardless of whether they use enclosing variables) are
+  //!    lifted (I'm not sure if this is actually an important thing to happen at
+  //!    the AST level, but it makes things easier).
+  //! 2. variables used in closures have their values wrapped in Cell, and their
+  //!    usage wrapped in DerefCell.
+  //! 3. Free variables in closure-functions get converted to parameters, which
+  //!    are expected to be passed as Cells.
+  //! 4. We set a local variable with the result of a MakeClosure where the inner
+  //!    function was defined.
+  //!
+  // TODO: Disallow any invocation or returning of a closure before all of its
+  // cells have been initialized.
+  // TODO: "drop" the (let cname (make-closure ...)) to the lowest point possible
+
   let mut result = vec![];
   for item in items {
     let out = match item {
@@ -69,14 +70,15 @@ pub fn closure_transform(items: &[AST]) -> Result<Vec<AST>, String> {
   Ok(result)
 }
 
+
 fn closurize_function(func: &Function) -> Result<Vec<AST>, String> {
-  // We need to do the following things:
-  // 1. look for inner functions
-  // 2. scan for their free variables
-  // 3. scan THIS function for matching variable declarations
-  // 4. If there are matches: wrap the declarations with (cell)
-  // 5. ALSO: only allow calls to the inner function if all the outer variables
-  //    have been initialized...
+  //! We need to do the following things:
+  //! 1. look for inner functions
+  //! 2. scan for their free variables
+  //! 3. scan THIS function for matching variable declarations
+  //! 4. If there are matches: wrap the declarations with (cell)
+  //! 5. ALSO: only allow calls to the inner function if all the outer variables
+  //!    have been initialized...
   let mut top_level = vec![];
   let code = {
     let mut transformer = |ast: &AST| -> Result<Option<AST>, String> {
@@ -100,7 +102,6 @@ fn closurize_function(func: &Function) -> Result<Vec<AST>, String> {
 // General transformation machinery
 
 
-// Transform an iterator of ASTs.
 fn transform_multi<'a, T>(
   asts: impl Iterator<Item = &'a AST>,
   transformer: &'a mut T,
@@ -108,15 +109,16 @@ fn transform_multi<'a, T>(
 where
   T: FnMut(&AST) -> Result<Option<AST>, String>,
 {
+  //! Transform an iterator of ASTs.
   asts.map(|ast| transform(ast, transformer)).collect()
 }
 
-/// Recursively transform an AST.
-/// If the visitor returns None, then the original AST is used and is then traversed.
 pub fn transform<T>(ast: &AST, transformer: &mut T) -> Result<AST, String>
 where
   T: FnMut(&AST) -> Result<Option<AST>, String>,
 {
+  //! Recursively transform an AST.
+  //! If the visitor returns None, then the original AST is used and is then traversed.
   let result = match transformer(ast)? {
     Some(replacement) => replacement,
     None => match ast {
