6bd96d3fffa68ecac52fd46f52e334a77e4e8d69
Christopher Armstrong <radix@twistedmatrix.com>
2018-09-19 21:48:57 -0500

    add pretty_assertions. very helpful for AST diffs!

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,10 @@ extern crate maplit;
 #[macro_use]
 extern crate serde_derive;
 
+#[cfg(test)]
+#[macro_use]
+extern crate pretty_assertions;
+
 pub mod builtins;
 pub mod compiler;
 pub mod interpreter;
